be960ea676e8cb96408bc057cb4af7d243d02d38
admin <admin@admins-Mac.local>
2014-12-28 12:16:09 -0800

    Initial Commit

---
+++ kndoo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kndoo
+//
+//  Created by admin on 12/28/14.
+//  Copyright (c) 2014 ___byabrik___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
